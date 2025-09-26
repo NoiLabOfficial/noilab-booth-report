f46b15bc31af9da92d81f2964c9bdedee0ad023e
NoiLabOfficial <noilab.official@gmail.com>
2025-09-26 16:08:38 +0900

    Update supabaseAdmin.ts

--- lib/supabaseAdmin.ts
+++ lib/supabaseAdmin.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
 const url = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-const service = process.env.SUPABASE_SERVICE_ROLE!;
+const service = process.env.SUPABASE_SERVICE_ROLE!; // 서버에서만 사용
 
 export const supabaseAdmin = createClient(url, service, {
   auth: { persistSession: false },
